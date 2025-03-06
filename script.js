4659ddc01e7ae8df205d89c27d61bbb978863e0b
Mohamed HAMMOU <147207511+Hmou05@users.noreply.github.com>
2025-03-06 00:45:16 +0000

    Navigation Bar --> 80% competed

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 let title = document.getElementById("title-card");
-let span = document.getElementsByTagName("span")
+let span = document.getElementsByTagName("span");
 
-
-function right(){
-    title.style = "text-shadow: 1vw 0px 0px black;"
+function right() {
+	title.style = "text-shadow: 1vw 0px 0px black;";
 }
 
-function left(){
-    title.style = "text-shadow: -1vw 0px 0px black;"
+function left() {
+	title.style = "text-shadow: -1vw 0px 0px black;";
 }
 
-function def(){
-    title.style = "text-shadow: 0px 0px 0px black;"
+function def() {
+	title.style = "text-shadow: 0px 0px 0px black;";
+}
+
+let dropdown = document.querySelector("#dropdown");
+let dropBtns = document.querySelector(".dropdown-btns");
+
+function changeDropIcon() {
+	if (dropdown.checked) {
+		dropBtns.innerHTML = `<svg class="dropdown-btn" xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>`;
+	} else {
+		dropBtns.innerHTML = `<svg class="dropdown-btn" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+            </svg>`;
+	}
 }
