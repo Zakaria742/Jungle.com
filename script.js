a9e661c48ccbe239ffc667ea88ddea1213d624b5
Zakaria742 <zaakizakaria12@gmail.com>
2025-03-06 13:04:40 +0000

    .

--- script.js
+++ script.js
@@ -1,32 +1,4 @@
 let title = document.getElementById("title-card");
-<<<<<<< HEAD
-let cursor = document.getElementById("cursor");
-let body = document.getElementById("body-main");
-//cursor pointer
-
-
-
-//body background
-
-body.addEventListener("load", () => {
-    body.style.backgroundSize = "cover";
-})
-
-var mouse = new MouseEvent(Number, {screenX, screenY});
-//setInterval(() => {console.log(mouse.movementX, mouse.movementY)}, 2000)
-//Main title animation
-function right(){
-    title.style = "text-shadow: 1vw 0px 0px var(--second-color);"
-}
-
-function left(){
-    title.style = "text-shadow: -1vw 0px 0px var(--second-color);"
-}
-
-function def(){
-    title.style = "text-shadow: 0px 0px 0px var(--second-color);"
-}
-=======
 let span = document.getElementsByTagName("span");
 
 function right() {
@@ -57,4 +29,3 @@ function changeDropIcon() {
             </svg>`;
 	}
 }
->>>>>>> 4659ddc01e7ae8df205d89c27d61bbb978863e0b
