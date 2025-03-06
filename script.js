89121a725afe884cd83521aaf121933a9609d549
Zakaria742 <zaakizakaria12@gmail.com>
2025-03-06 12:43:13 +0000

    Moved products section to another page

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 let title = document.getElementById("title-card");
-let span = document.getElementsByTagName("span")
+let cursor = document.getElementById("cursor");
+let body = document.getElementById("body-main");
+//cursor pointer
 
 
+
+//body background
+
+body.addEventListener("load", () => {
+    body.style.backgroundSize = "cover";
+})
+
+var mouse = new MouseEvent(Number, {screenX, screenY});
+//setInterval(() => {console.log(mouse.movementX, mouse.movementY)}, 2000)
+//Main title animation
 function right(){
-    title.style = "text-shadow: 1vw 0px 0px black;"
+    title.style = "text-shadow: 1vw 0px 0px var(--second-color);"
 }
 
 function left(){
-    title.style = "text-shadow: -1vw 0px 0px black;"
+    title.style = "text-shadow: -1vw 0px 0px var(--second-color);"
 }
 
 function def(){
-    title.style = "text-shadow: 0px 0px 0px black;"
+    title.style = "text-shadow: 0px 0px 0px var(--second-color);"
 }
