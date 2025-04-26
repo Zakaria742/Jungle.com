5383a729d30e6e9897bda6a6f5383d2ac3c4d072
Zakaria742 <zaakizakaria12@gmail.com>
2025-04-26 14:32:45 +0100

    Added a bunch of things

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let mainTitle = document.getElementById("main-title");
-let title = document.getElementById("title-card");
 let span = document.getElementsByTagName("span");
 let cursor = document.getElementById("cursor");
 let body = document.getElementById("body-main");
-let popupContainer= document.getElementById('popup-container');
+let popupContainer = document.getElementById('popup-container');
 let cards = document.querySelectorAll(".card");
 let innerCircle = document.getElementById("inner_circle");
 let offset = 10;
@@ -14,76 +13,19 @@ function lerp(start, end, time) {
 	return start + (end - start) * time;
 }
 
-function right() {
-	title.style = "text-shadow: 0.5vw -.1px 0px black;";
-}
-
-function left() {
-	title.style = "text-shadow: -0.5vw -.1px 0px black;";
-}
-
-function def() {
-	title.style = "text-shadow: 0px 0px 0px black;";
-}
-
-function show() {
-
-	body.style.opacity = "0";
-body.style.translate = "0rem -50vw";
-	setTimeout(() => {
-		body.style.opacity = "100%";
-		body.style.translate = "unset";
-	}, 10);
-
-	setTimeout(() => {
-		mainTitle.style.translate = "25vw 0px";
-		mainTitle.style.opacity = "1";
-	}, 2000)
-
-}
-if (!sessionStorage.getItem("show")) {
-
-		sessionStorage.setItem("show", true);
-		show();
-}
-
-function winEvent() {
-	mainTitle.style.opacity = "1";
-	if (window.innerWidth > 1026) {
-		mainTitle.style.translate = "25vw 0px";
-	}
-	else {
-
-		mainTitle.style.translate = `0px`;
-	}
-
-	requestAnimationFrame(winEvent)
-
-}
-if (mainTitle) {
-	winEvent();
-}
-
-
-
 
 document.addEventListener("mousemove", function (event) {
 
-	//console.log('Mouse X : ', event.clientX, 'Mouse Y : ', event.clientY);
 	targetX = event.clientX;
 	targetY = event.clientY;
 
-	//console.log("MouseX : ", cursor.style.left, "Mouse Y : ", cursor.style.top);
-
 })
 function update() {
-	const currentTop = parseFloat(cursor.style.top) || 0;
-	const currentLeft = parseFloat(cursor.style.left) || 0;
 
 	cursor.style.top = `${targetY - offset}px`;
 	cursor.style.left = `${targetX - offset}px`;
 
-	document.querySelectorAll('button, a, input, .stars').forEach(elements => {
+	document.querySelectorAll('button, a, input').forEach(elements => {
 		elements.onmouseenter = () => {
 			cursor.style.backgroundColor = "var(--main-color-transparent)";
 			cursor.style.border = "solid 1px black";
@@ -158,66 +100,101 @@ function changeDropIcon() {
 }
 
 
-const stars = document.querySelectorAll(".stars input");
-const submit = document.querySelector("button");
-const text = document.querySelector("textarea");
-
-stars.forEach((star) => {
-	star.addEventListener("change", function () {
-		const rating = this.value;
-
-		stars.forEach((star) => {
-			const label = document.querySelector(`label[for="${star.id}"]`);
-			label.style.color = star.value <= rating ? "gold" : "gray";
-		});
-	});
-});
-
-function clearTextrea() {
-	document.getElementById("description").value = "";
-}
-
-submit.addEventListener("click", () => {
-	alert(text.value);
-});
-
-
-
-
-
-
 //Pop up
 
 
 let element = document.getElementById("products")
-let picture = element.querySelectorAll("#pop-title>img")
+let picture = document.querySelectorAll("#pop-title>img")
 let pop = document.getElementById("popup")
 let description = document.getElementById("desc")
 
 let popTitle = document.querySelectorAll("#pop-title");
 
-function lerp(start, finish, time) {
-	return start + (finish - start) * time;
+async function fetchProduct() {
+	try {
+		const product = await fetch("./products.json");
+		const response = product.json();
+
+		return response;
+	}
+	catch (error) {
+		console.error(error);
+	}
+};
+
+function activatePopUp() {
+	popTitle.forEach(element => {
+		let img = element.querySelector("img");
+
+		element.onmouseover = () => {
+			let title = element.querySelector("h1");
+			pop.querySelector("h1").innerText = title.innerText;
+			pop.querySelector("img").setAttribute("src", img.currentSrc)
+			fetchProduct().then(data => {
+				data.forEach(d => {
+					if (d.name == title.textContent) {
+						pop.querySelector("p").textContent = d.description;
+						return 0;
+					}
+				})
+			});
+			pop.style.scale = "0.8";
+		}
+		element.onmouseout = () => {
+			pop.style.scale = "0";
+
+		}
+		document.addEventListener("mousemove", function (event) {
+			pop.style.left = `${targetX - 200}px`;
+			pop.style.top = `${targetY - 300}px`;
+		})
+	})
+}
+
+activatePopUp();
+
+
+//intersection
+const mainSection = document.getElementById("main-section");
+const introSection = document.getElementById("p-main");
+const introCards = document.querySelectorAll(".p1");
+const form = document.getElementById("formSection");
+
+const option = {
+	threshold: 0.2
+};
+const option2 = {
+	threshold: 0.3
+}
+function slideUpIntersection(entries, observer) {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.style.top = "4.5rem";
+			entry.target.style.opacity = "1";
+			observer.unobserve(mainSection);
+		}
+	})
+}
+
+function slideSidesIntersection(entries, observer) {
+	entries.forEach(entry => {
+		if (entry.isIntersecting) {
+			entry.target.style.left = "0";
+			entry.target.style.opacity = "1";
+		}
+	})
 }
 
 
-popTitle.forEach(element => {
-	let img = element.querySelector("img");
+let slideUp = new IntersectionObserver(slideUpIntersection, option);
+let slideLeft = new IntersectionObserver(slideSidesIntersection, option2);
+slideUp.observe(mainSection);
+slideUp.observe(introSection);
+slideUp.observe(form)
 
-	element.onmouseover = () => {
-		let title = element.querySelector("h1");
-		pop.querySelector("h1").innerText = title.innerText;
-		pop.querySelector("img").setAttribute("src", img.currentSrc)
-		pop.style.scale = "0.8";
-	}
-	element.onmouseout = () => {
-		pop.style.scale = "0";
+introCards.forEach( e => (slideLeft.observe(e)) );
 
-	}
-	document.addEventListener("mousemove", function (event) {
-		pop.style.left = `${targetX - 200}px`;
-		pop.style.top = `${targetY - 300}px`;
-	})
-})
+
+//fetching json for products
 
 
